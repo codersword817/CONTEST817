e7284ffc76e09b9008663e4b8bd093472e8309cc
codersword817 <marojuaakash2003@gmail.com>
2025-02-03 16:05:15 +0530

    changes made

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -22,7 +22,7 @@ const Home = () => {
 
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold mb-6 text-blue-600">Contest777</h1>
+      <h1 className="text-3xl font-bold mb-6 text-blue-600">Contest817</h1>
 
       <div className="card">
         <div className="mb-4">
